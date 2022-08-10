88d4222d14a097b3827924a2829434b52b4af1a3
David Sok <sokpheng7@yahoo.com>
2022-08-10 15:49:43 -0700

    changed contain_duplicate

--- contain_duplicate.py
+++ contain_duplicate.py
@@ -1,15 +1,31 @@
 from typing import DefaultDict
 
-def containDuplicate(nums):
+def containDuplicate(nums: list) -> bool:
     res = DefaultDict()
     for num in nums:
         tup = num
         if tup not in res:
             res[tup] = []
         res[tup].append(num)
-    print(res)
     if len(res[tup]) > 1:
         return True
     return False
 
-print(containDuplicate([1,2,3,1]))
+# Time Complexity = O(nlog(n))
+# Space Complexity = O(n)
+
+print(containDuplicate([1,2,3,2]))
+
+
+#VS
+
+def containDuplicate2(nums: list) -> bool:
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] == nums[j]:
+                return True
+    return False
+# Time Complexity = O(n^2)
+# Space Complexity = O(1)
+
+print(containDuplicate2([1,2,3,4]))    
