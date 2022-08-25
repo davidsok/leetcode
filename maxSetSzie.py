828879cce491ecca7cdbab6e8ea7cf1ef897c4b6
David Sok <sokpheng7@yahoo.com>
2022-08-24 17:29:56 -0700

    ...

--- maxSetSzie.py
+++ maxSetSzie.py
@@ -11,10 +11,9 @@ def maxSetSize(riceBags):
                 res[v].append(w)
             if res[v][-1] ** 2 == w:
                 res[v].append(w)
-           
+    print(res)       
     ans = max(len(x)for x in list(res.values()))
     print(ans)
-
     return ans
 maxSetSize([625,4,2,5,25,26])  # OUTPUT = 3
 maxSetSize([625,16,4,2,25,256,65536]) # OUTPUT = 5
