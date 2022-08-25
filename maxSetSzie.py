07dd48c1fcba85bb2aec5df40caeb37bcf290783
David Sok <sokpheng7@yahoo.com>
2022-08-24 17:21:57 -0700

    just

--- maxSetSzie.py
+++ maxSetSzie.py
@@ -2,19 +2,22 @@ def maxSetSize(riceBags):
     res = {}
     rb = sorted(riceBags)
     if not riceBags:
+        print(-1)
         return -1
-    for i in range(len(rb)):
-        res[rb[i]] = [rb[i]]
-        for j in range(len(rb)):
-            if rb[i] ** 2 == rb[j]:
-                res[rb[i]].append(rb[j])
-            if res[rb[i]][len(res[rb[i]])-1] ** 2 == rb[j]:
-                res[rb[i]].append(rb[j])
+    for v in rb:
+        res[v] = [v]
+        for w in rb:
+            if v ** 2 == w:
+                res[v].append(w)
+            if res[v][-1] ** 2 == w:
+                res[v].append(w)
            
     ans = max(len(x)for x in list(res.values()))
     print(ans)
 
     return ans
-maxSetSize([625,4,2,5,25,26])
-maxSetSize([625,16,4,2,25,256])
-maxSetSize([5,4,2,15,25])    
+maxSetSize([625,4,2,5,25,26])  # OUTPUT = 3
+maxSetSize([625,16,4,2,25,256,65536]) # OUTPUT = 5
+maxSetSize([5,4,2,15,25])   #OUPUT = 2
+maxSetSize([3,9,81,6561, 1, 2]) #OUTPUT 4
+maxSetSize([])       #OUTPUT -1
