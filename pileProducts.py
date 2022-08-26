129e64c4d8776a0b69c27e64ac1ecacee52d2bb2
David Sok <davidsok7@yahoo.com>
2022-08-26 11:10:52 -0700

    826

--- pileProducts.py
+++ pileProducts.py
@@ -7,13 +7,18 @@ def findMaxProducts(products):
     current = products[maxIndex]
     output.append(current)
     for i in range(maxIndex, minIndex, -1):
-        if products[i-1] > current:
-            output.append(current - 1)
+        if products[i-1] == current:
+            output.insert(0, current - 1)
+            current = output[len(output)-1]
+        elif products[i-1] < current:
+            current = products[i-1]
+            output.insert(0,current)
             current = output[len(output)-1]
         else:
-            current = products[i-1]
-            output.append(current-1)
+            current = products[i]
+            output.insert(0,current-1)
             current = output[len(output)-1]
+            # print(output)
     print(output)
     return output
 
